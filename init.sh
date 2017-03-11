8c3ed3397b74e99855004524c28287db591d4b17
fengb <contact@fengb.info>
2017-03-11 17:37:38 -0600

    Remove obsolete links

--- init.sh
+++ init.sh
@@ -4,9 +4,8 @@
 #exec 3>&2 2>trace.log
 #set -x
 
-dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"/home && pwd -P)"
-#echo "$(cd "`dirname "${BASH_SOURCE[0]}`" && git rev-parse --show-toplevel)"
-#exit
+base="$(cd "`dirname "${BASH_SOURCE[0]}"`" && git rev-parse --show-toplevel)"
+dir="$base/home"
 
 print0() {
   tr $'\r\n' "\0\0"
@@ -15,7 +14,13 @@ print0() {
 find_filter() {
   files="`cat`"
   [ -z "$files" ] && return
-  print0 <<< "$files" | xargs -0 -J {} find {} -maxdepth 0 "$@"
+  if [ "$1" = "-L" ]; then
+    arg="$1"
+    shift
+    print0 <<< "$files" | xargs -0 -J {} find "$arg" {} -maxdepth 0 "$@" 2> /dev/null
+  else
+    print0 <<< "$files" | xargs -0 -J {} find {} -maxdepth 0 "$@" 2> /dev/null
+  fi
 }
 
 as_home() {
@@ -28,10 +33,10 @@ as_src() {
 }
 
 rm_files() {
-  files="${1-`cat`}"
+  files="`concat "$@" | sort -u`"
   [ -z "$files" ] && return
-  sort <<< "$files"
-  read -p "About to delete the above [enter to continue]: "
+  echo "$files"
+  read -p "About to delete [enter to continue]: "
   echo
 
   print0 <<< "$files" | xargs -0 rm -rf
@@ -47,6 +52,10 @@ run_log() {
   echo "$@" && "$@"
 }
 
+git_historic_files() {
+  git log --pretty=format: --name-only --diff-filter=A | sed -e '/^ *$/d' -e "s;^;$base/;" | sort -u
+}
+
 #find -L "$HOME" -not \( -not -path "$HOME" -prune \) -type l
 
 dirnew="`find "$dir" -not \( -path '*.link' -prune \) -type d | as_home`"
@@ -54,8 +63,9 @@ links="`find "$dir" \( -type d -name '*.link' -prune \) -or -type f | as_home`"
 
 existdirs="`find_filter -not -type d <<< "$dirnew"`"
 existlinks="`find_filter -not -type l <<< "$links"`"
+brokelinks="`git_historic_files | as_home | find_filter -L -maxdepth 0 -type l`"
 
-concat "$existdirs" "$existlinks" | rm_files
+rm_files "$existdirs" "$existlinks" "$brokelinks"
 
 while read -r dst; do
   [ ! -e "$dst" ] && run_log mkdir -p "$dst"
